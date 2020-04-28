f3ad8ac41ca004f525a0141fd0279663b24e0db4
liuxinhongg <644391351@qq.com>
2020-04-28 12:02:40 +0800

    vue 组件和路由

---
+++ vueExample/vuethree/src/components/page.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h3>
+            <font color="red">{{info}}</font>
+        </h3>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                info: "我是page页面"
+            }
+        },
+    }
+</script>
+<style>
+
+</style>
--- vueExample/vuethree/src/router/index.js
+++ vueExample/vuethree/src/router/index.js
@@ -1,15 +1,66 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+import Page from '@/components/page'
+import Work from '@/components/work'
+import frist from '@/components/frist'
+const User = {
+    template: `<div>
+    <h3><font color="green">我是父组件 {{$route.params.id}} </font></h3>
+    <router-view/>
+    </div>`
+}
+const Profile = {
+    template: `<div><h3><font color="red">我是Profile子组件 </font></h3></div>`
+}
+const Posts = {
+    template: `<div><h3><font color="green">我是posts组件 </font></h3></div>`
+}
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: 'history',
+    linkExactActiveClass: 'nav',
+    routes: [{
+            path: '/',
+            name: 'HelloWorld',
+            component: HelloWorld
+        },
+        {
+            path: "/page",
+            name: "Page",
+            component: Page
+        },
+        {
+            path: "/work",
+            name: "Work",
+            component: Work
+        },
+        {
+            path: "/frist/:id",
+            name: "frist",
+            component: frist
+        },
+        {
+            path: '/user/:id',
+            name: "User",
+            component: User,
+            children: [
+                //  {
+                //    path: 'profile',
+                //    name: "Profile",
+                //    component: Profile,
+                //  },
+                {
+                    path: '',
+                    name: "Profile",
+                    component: Profile,
+                }, {
+                    path: '/posts',
+                    name: "Posts",
+                    component: Posts,
+                }
+            ]
+        }
+    ]
 })
